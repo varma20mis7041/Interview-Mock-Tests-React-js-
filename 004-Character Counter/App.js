b74817a9ec177812f82851bc05dca6ac115fac92
Bhargav varma <80447853+varma20mis7041@users.noreply.github.com>
2024-02-15 15:47:26 +0530

    Create App.js

---
+++ 004-Character Counter/App.js
@@ -0,0 +1,17 @@
+import './App.css'
+import CharacterCounter from './components/CharacterCounter'
+
+const App = () => (
+  <>
+    <CharacterCounter />
+
+    <div className="last_container">
+      <p>
+        Developed by @Bhargav varma | Date of completion : 15 Feb 2024 | contact
+        : bhargav.coding@gmail.com{' '}
+      </p>
+    </div>
+  </>
+)
+
+export default App
