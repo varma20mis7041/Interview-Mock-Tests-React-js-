1330caf9a9e02e373bff98812bdfae28ec46bb41
Bhargav varma <80447853+varma20mis7041@users.noreply.github.com>
2024-02-27 21:41:49 +0530

    Create index.js

---
+++ 012-Meetup/components/Header/index.js
@@ -0,0 +1,12 @@
+import './index.css'
+
+const Header = () => (
+  <nav className="nav_container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/meetup/website-logo-img.png"
+      alt="website logo"
+      className="logo_element"
+    />
+  </nav>
+)
+export default Header
